a78141f7ab355b4c6ee9e2888f88661af85bd111
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-13 11:21:00 +0900

    PassJudge02.javaを追加

---
+++ PassJudge02/PassJudge02.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PassJudge02 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("点数を入力>");
+		int score = Integer.parseInt(br.readLine());
+		
+		if(score < 70) {
+			System.out.println("不合格です");
+		} else {
+			System.out.println("合格です");
+		}
+	}
+}
