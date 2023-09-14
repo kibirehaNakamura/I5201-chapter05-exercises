f9b65be9f48362b11cd702de2601574b15529092
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-14 10:53:47 +0900

    Multiple2.javaを追加

---
+++ Multiple2/Multiple2.java
@@ -0,0 +1,14 @@
+public class Multiple2 {
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				if((i * j) < 10) {
+					System.out.print("  " + (i * j));
+				} else {
+					System.out.print(" " + (i * j));
+				}
+			}
+				System.out.println();
+		}
+	}
+}
